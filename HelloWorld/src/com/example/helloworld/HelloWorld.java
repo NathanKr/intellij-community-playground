47b0461a89023fa8b9ce5ce40927549ddba05d6d
Nathan Krasney <natankrasney@gmail.com>
2020-07-16 10:05:42 +0300

    small source fix

--- HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,6 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Person p = new Person("Nathan" , 57);
-        p.write();
+        System.out.println("Hello World");
     }
 }
